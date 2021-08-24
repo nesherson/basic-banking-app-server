b0f4c66bf2ad0ea3699eaa342f3ad30f7a42be00
nesherson <nesad.omeragich27@gmail.com>
2021-08-25 01:12:11 +0200

    Add users controller for getting mock data

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using basic_banking_app_server.Models;
+using basic_banking_app_server.Data;
+
+namespace basic_banking_app_server.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepo _repository;
+
+        public UsersController(IUserRepo repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetAllUsers()
+        {
+            var users = _repository.GetAllUsers();
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUserById(int id)
+        {
+            var user = _repository.GetUserById(id);
+
+            return Ok(user);
+        }
+
+    }
+}
