5f3b15ca7ca42960a2f0e5154105e247d77cb62d
nesherson <nesad.omeragich27@gmail.com>
2021-08-25 00:42:12 +0200

    Add user model

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace basic_banking_app_server.Models
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+    }
+}
