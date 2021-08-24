791f4bef233e354d1a63a2bd594d8a288f38cd86
nesherson <nesad.omeragich27@gmail.com>
2021-08-25 01:12:52 +0200

    Add mock user repository

---
+++ Data/MockUserRepo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using basic_banking_app_server.Models;
+
+namespace basic_banking_app_server.Data
+{
+    public class MockUserRepo : IUserRepo
+    {
+
+        private List<User> users = new List<User>
+            {
+                new User { Id = 0, Email = "neero@test.com", FirstName = "Neero", LastName = "Tantrum", Password = "123neero" },
+                new User { Id = 1, Email = "adam@test.com", FirstName = "Adam", LastName = "Adamson", Password = "123adam" },
+                new User { Id = 2, Email = "daniel@test.com", FirstName = "Daniel", LastName = "Damner", Password = "123daniel" },
+                new User { Id = 3, Email = "tim@test.com", FirstName = "Tim", LastName = "Goliath", Password = "123tim" }
+
+            };
+        public IEnumerable<User> GetAllUsers()
+        {
+            return users;
+        }
+
+        public User GetUserById(int id)
+        {
+            var user = users.Find(user => user.Id == id);
+
+            return user;
+        }
+    }
+}
