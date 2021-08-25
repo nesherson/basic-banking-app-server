448d2aa4ee11bddd981a497fa01eec4ccae19455
nesherson <nesad.omeragich27@gmail.com>
2021-08-25 04:40:41 +0200

    Add controllers and routes for creating and updating users

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
 
 using basic_banking_app_server.Models;
 using basic_banking_app_server.Data;
@@ -35,7 +36,7 @@ namespace basic_banking_app_server.Controllers
             return Ok(dtoUsers);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetUserById")]
         public ActionResult<User> GetUserById(int id)
         {
             var user = _repository.GetUserById(id);
@@ -47,6 +48,41 @@ namespace basic_banking_app_server.Controllers
 
             return Ok(dtoUser);
         }
+
+        [HttpPost]
+        public ActionResult<UserReadDto> CreateUser(UserCreateDto userCreateDto)
+        {
+            var userModel = _mapper.Map<User>(userCreateDto);
+            _repository.CreateUser(userModel);
+            _repository.SaveChanges();
+
+            var userReadDto = _mapper.Map<UserReadDto>(userModel);
+
+            return CreatedAtRoute(nameof(GetUserById), new { id = userReadDto.Id }, userReadDto);
+        }
+
+        [HttpPatch("{id}")]
+        public ActionResult UpdateUser(int id, JsonPatchDocument<UserUpdateDto> patchDoc)
+        {
+            var userModelFromRepo = _repository.GetUserById(id);
+
+            if (userModelFromRepo == null)
+                return NotFound();
+
+            var userToPatch = _mapper.Map<UserUpdateDto>(userModelFromRepo);
+            patchDoc.ApplyTo(userToPatch, ModelState);
+
+            if (!TryValidateModel(userToPatch))
+                return ValidationProblem(ModelState);
+
+            _mapper.Map(userToPatch, userModelFromRepo);
+            _repository.SaveChanges();
+
+            return NoContent();
+
+        }
+
+
 
     }
 }
