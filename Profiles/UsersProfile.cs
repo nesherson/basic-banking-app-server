4c88bb71692a05a041e3d7a71d51adb15016863a
nesherson <nesad.omeragich27@gmail.com>
2021-08-25 02:51:43 +0200

    Add mapping profile for getting user data

---
+++ Profiles/UsersProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+
+using basic_banking_app_server.Models;
+using basic_banking_app_server.Dtos;
+
+namespace basic_banking_app_server.Profiles
+{
+    public class UsersProfile : Profile
+    {
+        public UsersProfile()
+        {
+            CreateMap<User, UserReadDto>();
+        }
+    }
+}
