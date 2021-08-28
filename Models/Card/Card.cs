6bef24746607eb0b5ad6594d650d112a52f905f3
nesherson <nesad.omeragich27@gmail.com>
2021-08-28 23:58:42 +0200

    File and folder restructure

---
+++ Models/Card/Card.cs
@@ -0,0 +1,16 @@
+﻿using basic_banking_app_server.Models.UserModel;
+
+namespace basic_banking_app_server.Models.CardModel
+{ 
+    public partial class Card
+    {
+        public int Id { get; set; }
+        public string CardNumber { get; set; }
+        public string Type { get; set; }
+        public string Network { get; set; }
+        public decimal? Balance { get; set; }
+        public int? UserId { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ Models/User/User.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+using basic_banking_app_server.Models.CardModel;
+
+namespace basic_banking_app_server.Models.UserModel
+{
+    public partial class User
+    {
+        public User()
+        {
+            Cards = new HashSet<Card>();
+        }
+
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+
+        public virtual ICollection<Card> Cards { get; set; }
+    }
+}
