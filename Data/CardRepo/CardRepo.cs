dbbb648a31a770b703a2a8ad71ce6f5c268aef1c
nesherson <nesad.omeragich27@gmail.com>
2021-08-28 03:48:55 +0200

    Add card interface and repo

---
+++ Data/CardRepo/CardRepo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using basic_banking_app_server.Models.CardModel;
+using basic_banking_app_server.Data.Context;
+
+namespace basic_banking_app_server.Data.CardRepo
+{
+    public class CardRepo : ICardRepo
+    {
+        private readonly BasicBankContext _context;
+
+        public CardRepo(BasicBankContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Card> GetAllCards()
+        {
+            var cards = _context.Cards.ToList();
+
+            return cards;
+        }
+
+        public Card GetCardById(int id)
+        {
+            var card = _context.Cards.FirstOrDefault(card => card.Id == id);
+
+            return card;
+        }
+    }
+}
---
+++ Data/CardRepo/ICardRepo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+using basic_banking_app_server.Models.CardModel;
+
+namespace basic_banking_app_server.Data.CardRepo
+{
+    public interface ICardRepo
+    {
+        IEnumerable<Card> GetAllCards();
+        Card GetCardById(int id);
+    }
+}
