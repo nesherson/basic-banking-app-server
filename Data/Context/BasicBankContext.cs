f21c54510f5ef5243542cf92c9d734c72df953fc
nesherson <nesad.omeragich27@gmail.com>
2021-08-28 02:32:48 +0200

    Update ef context with card model

--- Data/Context/BasicBankContext.cs
+++ Data/Context/BasicBankContext.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
 
 using basic_banking_app_server.Models;
 
@@ -17,6 +15,7 @@ namespace basic_banking_app_server.Data.Context
         {
         }
 
+        public virtual DbSet<Card> Cards { get; set; }
         public virtual DbSet<User> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -30,6 +29,30 @@ namespace basic_banking_app_server.Data.Context
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "English_United States.1252");
+
+            modelBuilder.Entity<Card>(entity =>
+            {
+                entity.ToTable("cards");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Balance).HasColumnName("balance");
+
+                entity.Property(e => e.CardNumber)
+                    .HasMaxLength(16)
+                    .HasColumnName("card_number");
+
+                entity.Property(e => e.Network).HasColumnName("network");
+
+                entity.Property(e => e.Type).HasColumnName("type");
+
+                entity.Property(e => e.UserId).HasColumnName("user_id");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Cards)
+                    .HasForeignKey(d => d.UserId)
+                    .HasConstraintName("cards_user_id_fkey");
+            });
 
             modelBuilder.Entity<User>(entity =>
             {
