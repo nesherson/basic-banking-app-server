4074813806780d29c0499e9e91bd3401efaee0d7
nesherson <nesad.omeragich27@gmail.com>
2021-08-29 00:08:53 +0200

    Update user and card models

--- Models/Card/Card.cs
+++ Models/Card/Card.cs
@@ -1,9 +1,20 @@
-﻿using basic_banking_app_server.Models.UserModel;
+﻿using System.Collections.Generic;
+
+using basic_banking_app_server.Models.UserModel;
+using basic_banking_app_server.Models.TransactionModel;
 
 namespace basic_banking_app_server.Models.CardModel
 { 
     public partial class Card
     {
+        public Card()
+        {
+            TransactionDeposits = new HashSet<TransactionDeposit>();
+            TransactionPaymentReceiverCards = new HashSet<TransactionPayment>();
+            TransactionPaymentSenderCards = new HashSet<TransactionPayment>();
+            TransactionWithdrawals = new HashSet<TransactionWithdrawal>();
+        }
+
         public int Id { get; set; }
         public string CardNumber { get; set; }
         public string Type { get; set; }
@@ -12,5 +23,9 @@ namespace basic_banking_app_server.Models.CardModel
         public int? UserId { get; set; }
 
         public virtual User User { get; set; }
+        public virtual ICollection<TransactionDeposit> TransactionDeposits { get; set; }
+        public virtual ICollection<TransactionPayment> TransactionPaymentReceiverCards { get; set; }
+        public virtual ICollection<TransactionPayment> TransactionPaymentSenderCards { get; set; }
+        public virtual ICollection<TransactionWithdrawal> TransactionWithdrawals { get; set; }
     }
 }
--- Models/User/User.cs
+++ Models/User/User.cs
@@ -16,6 +16,9 @@ namespace basic_banking_app_server.Models.UserModel
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Password { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
 
         public virtual ICollection<Card> Cards { get; set; }
     }
