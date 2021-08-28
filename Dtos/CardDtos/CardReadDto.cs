4812218f48ffc325cf1646c59d432b38d402f193
nesherson <nesad.omeragich27@gmail.com>
2021-08-28 03:49:12 +0200

    Add card read dto

---
+++ Dtos/CardDtos/CardReadDto.cs
@@ -0,0 +1,12 @@
+﻿namespace basic_banking_app_server.Dtos.CardDtos
+{
+    public class CardReadDto
+    {
+        public int Id { get; set; }
+        public string CardNumber { get; set; }
+        public string Type { get; set; }
+        public string Network { get; set; }
+        public decimal? Balance { get; set; }
+        public int? UserId { get; set; }
+    }
+}
