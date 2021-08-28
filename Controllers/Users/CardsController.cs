f1bf5592da84779c202bcd9ce3caa49e30722182
nesherson <nesad.omeragich27@gmail.com>
2021-08-28 03:48:38 +0200

    Add card controller

---
+++ Controllers/Users/CardsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+
+using basic_banking_app_server.Data.CardRepo;
+using basic_banking_app_server.Models.CardModel;
+using basic_banking_app_server.Dtos.CardDtos;
+
+namespace basic_banking_app_server.Controllers.Users
+{
+    [Authorize]
+    [Route("cards")]
+    [ApiController]
+    public class CardsController : ControllerBase
+    {
+        private readonly ICardRepo _cardRepo;
+        private readonly IMapper _mapper;
+        public CardsController(ICardRepo cardRepo, IMapper mapper)
+        {
+            _cardRepo = cardRepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Card>> GetAllCards()
+        {
+            var cards = _cardRepo.GetAllCards();
+
+            var cardsReadModel = _mapper.Map<IEnumerable<CardReadDto>>(cards);
+
+            return Ok(cardsReadModel);
+        }
+    }
+}
