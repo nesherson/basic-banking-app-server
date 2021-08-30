a328cd2e19e400480978d29966660c07ed3bec5c
nesherson <nesad.omeragich27@gmail.com>
2021-08-30 06:06:21 +0200

    Add enums for transaction method and status

---
+++ Enums/TransactionEnums.cs
@@ -0,0 +1,8 @@
+﻿namespace basic_banking_app_server.Enums
+{
+    public class TransactionEnums
+    {
+        public enum Method { deposit, withdraw, payment }
+        public enum Status { pending, failed, captured, refunded }
+    }
+}
