daee24990068e77aabd2533c6b99f6d97ffe90b1
nesherson <nesad.omeragich27@gmail.com>
2021-08-30 18:13:41 +0200

    Add transaction mapping profile

---
+++ Profiles/TransactionsProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+using basic_banking_app_server.Models.TransactionModel;
+using basic_banking_app_server.Dtos.TransactionDtos;
+using basic_banking_app_server.Enums;
+
+namespace basic_banking_app_server.Profiles
+{
+    public class TransactionProfile : Profile
+    {
+        public TransactionProfile()
+        { 
+            CreateMap<TransactionDepositCreateDto, Transaction>().ConstructUsing(e => new Transaction(e.Amount, TransactionEnums.Method.deposit, e.CardId));
+            CreateMap<TransactionWithdrawCreateDto, Transaction>().ConstructUsing(e => new Transaction(e.Amount, TransactionEnums.Method.withdraw, e.CardId));
+        }
+    }
+}
