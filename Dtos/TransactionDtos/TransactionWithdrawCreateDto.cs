3557f04b0d3cc422938d8a04b54fc197762b712c
nesherson <nesad.omeragich27@gmail.com>
2021-08-30 18:14:24 +0200

    Add transaction create dto for deposit and withdraw

---
+++ Dtos/TransactionDtos/TransactionWithdrawCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace basic_banking_app_server.Dtos.TransactionDtos
+{
+    public class TransactionWithdrawCreateDto
+    {
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public int CardId { get; set; }
+    }
+}
